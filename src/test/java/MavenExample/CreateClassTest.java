2b4b0684e899bf729f93c5ad4f185aeb72effc9d
AnushaThogaru <anushaverpula@gmail.com>
2022-05-10 15:19:22 +0530

    Mavensample

---
+++ src/test/java/MavenExample/CreateClassTest.java
@@ -0,0 +1,13 @@
+package MavenExample;
+
+
+	import org.testng.annotations.Test;
+
+	public class CreateClassTest {
+		@Test
+		public void createusertest()
+		{
+			System.out.println("creating a user");	
+		}
+	}
+
